7f1d20be7511cf789cd49ce3d5bae89daa8354c4
raerek <raerek@outlook.com>
2022-01-13 11:38:55 +0100

    showing hostname in footer

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
+
+HOSTNAME = os.uname().nodename # Does not work on Windows.
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
@@ -9,7 +12,7 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String, nullable=False)
+    content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -29,7 +32,8 @@ def index():
             return 'Nem sikerült a feladat felvétele.'
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        return render_template('index.html', tasks=tasks,
+            message=f'kiszolgáló: {HOSTNAME}')
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -53,6 +57,7 @@ def update(id):
             return 'Nem sikerült a feladat frissítése.'
     else:
         
-        return render_template('update.html', task=task_to_update)
+        return render_template('update.html', task=task_to_update,
+            message=f'kiszolgáló: {HOSTNAME}')
 if __name__ == '__main__':
     app.run(debug = True)
